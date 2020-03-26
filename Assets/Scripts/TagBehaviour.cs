45940ead32588eb919b8456dde57d062e933cde4
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-03-26 18:06:32 -0500

    Redoing the AI

---
+++ Assets/Scripts/TagBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagBehaviour : MonoBehaviour
+{
+    public const string player = "Player";
+    public const string monster = "Monster";
+}
