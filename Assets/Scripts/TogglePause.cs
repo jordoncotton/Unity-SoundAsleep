42e5003a4a070164676618486969a7800a3155ef
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-03-31 15:49:31 -0500

    These are to be deleted soon except the toggle pause

---
+++ Assets/Scripts/TogglePause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cotton
+{
+    public class TogglePause : MonoBehaviour
+    {
+        private GameManagerMaster gameManagerMaster;
+        private bool isPaused;
+
+        void OnEnable()
+        {
+            
+        }
+
+        void OnDisable()
+        {
+            
+        }
+
+        void SetInitialReferences()
+        {
+
+        }
+
+        void TogglePause()
+        {
+
+        }
+    }
+}
+
