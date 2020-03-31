77cb49889c699eceff619601aa0a46d6b3f0be53
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-03-31 14:05:39 -0500

    Working AI States

--- Assets/Scripts/Monser AI Scrips/MonsterBehaviour3.cs
+++ Assets/Scripts/Monser AI Scrips/MonsterBehaviour3.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterBehaviour3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 2f;
+    public float speed2 = 5f;
+    public GameObject[] Waypoint;
+    public float searchTime = 5f;
+    public float patrolTime;
+
+    private NavMeshAgent nav;
+    private int curntPoint = 0;
+
+    void Awake()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Waypoint.Length == curntPoint)
+        {
+            Searching();
+        }
+
+        else
+        Patrolling();
+    }
+
+    void Patrolling()
+    {
+        nav.speed = speed;
+
+        nav.destination = Waypoint[curntPoint].transform.position;
+
+        if (curntPoint == Waypoint.Length-1)
+        {
+            curntPoint = 0;
+        }
+
+        else
+        {
+            curntPoint++;
+        }
+    }
+
+    void Searching()
+    {
+        searchTime += Time.deltaTime;
+
+        if (searchTime > patrolTime)
+        {
+            searchTime = 0f;
+        }
     }
 }
