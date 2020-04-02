373c626dea5a8f98ec3855a5b7b73f537e690dab
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-04-02 17:15:08 -0500

    Working on players inventory to be usable and detecting an item when picking it up.

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cotton
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        public Transform inventoryPlayerParent;
+        public Transform inventoryUIParent;
+        public GameObject uiButton;
+
+        private PlayerMaster playerMaster;
+        private GameManagerToggleInventoryUI inventoryUIScript;
+        private float timeToPlaceInHands = 0.1f;
+        private Transform currentlyHeldItem;
+        private int counter string buttonText;
+    }
+}
+
---
+++ Assets/Scripts/PlayerMaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cotton
+{
+    public class PlayerMaster : MonoBehaviour
+    {
+        public delegate void GeneralEventHandler();
+        public event GeneralEventHandler EventInventoryChanged;
+        public event GeneralEventHandler EventHandsEmpty;
+
+        public delegate void PickedupEventHandler(string PickedupName, int quantity);
+        public event PickedupEventHandler EventPickedup;
+        
+        public void CallEventInventoryChanged()
+        {
+            if(EventInventoryChanged != null)
+            {
+                EventInventoryChanged();
+            }
+        }
+
+        public void CallEventHandsEmpty()
+        {
+            if(EventHandsEmpty != null)
+            {
+                EventHandsEmpty();
+            }
+        }
+
+        public void CallEventPickedup(string PickedupName, int quantity)
+        {
+            if(EventPickedup != null)
+            {
+                EventPickedup(PickedupName, quantity);
+            }
+        }
+    }
+}
+
