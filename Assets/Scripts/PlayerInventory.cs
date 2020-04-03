04da26d7e8fa22b4116b0d8b6e0f7d671c9b2606
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-04-02 19:37:10 -0500

    Working on completing the player inventory script.

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -15,7 +15,62 @@ namespace Cotton
         private GameManagerToggleInventoryUI inventoryUIScript;
         private float timeToPlaceInHands = 0.1f;
         private Transform currentlyHeldItem;
-        //private int counter string buttonText;
+        private int counter;
+        private string buttonText;
+        private List<Transform> listInventory = new List<Transform>();
+
+        void OnEnable()
+        {
+            SetInitialReferences();
+            UpdateInventoryListAndUI();
+            CheckIfHandsEmpty();
+        }
+
+        void OnDisable()
+        {
+            
+        }
+
+        void SetInitialReferences()
+        {
+            inventoryUIScript = GameObject.Find("GameManager").GetComponent<GameManagerToggleInventoryUI>();
+            playerMaster = GetComponent<PlayerMaster>();
+        }
+
+        void UpdateInventoryListAndUI()
+        {
+
+        }
+
+        void CheckIfHandsEmpty()
+        {
+
+        }
+
+        void ClearHands()
+        {
+
+        }
+
+        void ClearInventoryUI()
+        {
+
+        }
+
+        public void ActiveInventoryItem(int inventoryIndex)
+        {
+
+        }
+
+        void DeactiveAllInventoryItems()
+        {
+
+        }
+
+        IEnumerator PlaceItemInHands(Transform itemTransform)
+        {
+            yield return new WaitForSeconds(timeToPlaceInHands);
+        }
     }
 }
 
