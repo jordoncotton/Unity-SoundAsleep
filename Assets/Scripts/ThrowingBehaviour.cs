3feb136b5c6dd70ed87326f92f9867ae1ba6f17b
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-04-28 17:55:49 -0500

    got the arc working, need to complete pickups.

---
+++ Assets/Scripts/ThrowingBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingBehaviour : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    public GameObject player;
+
+    public Rigidbody ballRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.SetActive(false);
+
+        ballRB = ball.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ball.SetActive(true);
+            ball.transform.rotation = player.transform.rotation;
+            ballRB.AddForce(new Vector3(4, 4, 0), ForceMode.Impulse);
+
+        }
+    }
+}
