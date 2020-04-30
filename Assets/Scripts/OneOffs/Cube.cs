d8760899fb23f1e1b33119be040cab5c48f1e525
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-04-30 10:30:29 -0500

    throwing works

---
+++ Assets/Scripts/OneOffs/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    public Rigidbody cubeRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("l");
+
+        if (other.gameObject.tag == "ThrowArc" && cubeRB.isKinematic == false)
+        {
+            cubeRB.isKinematic = true;
+        }
+    }
+}
--- Assets/Scripts/ThrowingBehaviour.cs
+++ Assets/Scripts/ThrowingBehaviour.cs
@@ -7,26 +7,106 @@ public class ThrowingBehaviour : MonoBehaviour
 
     public GameObject ball;
 
+    public GameObject cube;
+
+    public GameObject ballholdr;
+
+    public GameObject cubeholdr;
+
     public GameObject player;
 
     public Rigidbody ballRB;
+
+    public Rigidbody cubeRB;
+
+    public bool isHolding;
+
+    public bool BallThrown;
+
+    public bool CubeThrown;
+
+   // public Mesh ballMsh;
+
+   // public Mesh cubeMsh;
     // Start is called before the first frame update
     void Start()
     {
         ball.SetActive(false);
 
+        cube.SetActive(false);
+
+        //ballMsh = false;
+
         ballRB = ball.GetComponent<Rigidbody>();
+
+        cubeRB = cube.GetComponent<Rigidbody>();
+
+        ballRB.isKinematic = true;
+
+        cubeRB.isKinematic = true;
+
+        isHolding = false;
+
+        BallThrown = true;
+
+        CubeThrown = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && ballholdr.activeInHierarchy == false && BallThrown == false)
         {
-            ball.SetActive(true);
-            ball.transform.rotation = player.transform.rotation;
+            ball.transform.parent = null;
+            ballRB.isKinematic = false;
+            //ball.transform.rotation = player.transform.rotation;
             ballRB.AddForce(new Vector3(4, 4, 0), ForceMode.Impulse);
 
+            isHolding = false;
+
+            BallThrown = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && cubeholdr.activeInHierarchy == false && CubeThrown == false)
+        {
+
+            cube.transform.parent = null;
+            cubeRB.isKinematic = false;
+            //cube.transform.rotation = player.transform.rotation;
+            cubeRB.AddForce(new Vector3(4, 4, 0), ForceMode.Impulse);
+
+            isHolding = false;
+
+            CubeThrown = true;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("p");
+
+        if(other.gameObject.tag == "CUBE" && isHolding == false)
+        {
+            cubeholdr.SetActive(false);
+
+            isHolding = true;
+
+            cube.SetActive(true);
+
+            CubeThrown = false;
+
+        }
+
+        if (other.gameObject.tag == "BALL" && isHolding == false)
+        {
+            ballholdr.SetActive(false);
+
+            isHolding = true;
+
+            ball.SetActive(true);
+
+            BallThrown = false;
         }
     }
 }
