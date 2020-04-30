0e926a5e1dcef906e00d29f49d9ee35b58c3d0b0
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-04-29 23:23:38 -0500

    Commented out the code to work on other things for the time being

--- Assets/Scripts/CarryItem.cs
+++ Assets/Scripts/CarryItem.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CarryItem : MonoBehaviour
-{
+//public class CarryItem : MonoBehaviour
+//{
     
-}
+//}
