3f9fb3f253a078049b74c8e716d0038f6dd16324
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-05-15 01:51:30 -0500

    Getting together scripts for picking up aan object then storing it.

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -2,34 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Cotton
-{
-    public class ItemPickup : MonoBehaviour
-    {
-        private ItemMaster itemMaster;
+//public class ItemPickup : MonoBehaviour
+//{
+//    private Inventory inventory;
+//    public    
+//}
 
-        void OnEnable()
-        {
-            SetInitialReferences();
-            itemMaster.EventPickupAction += CarryOutPickupActions;
-        }
-
-        void OnDisable()
-        {
-            itemMaster.EventPickupAction -= CarryOutPickupActions;
-        }
-
-        void SetInitialReferences()
-        {
-            itemMaster = GetComponent<ItemMaster>();
-        }
-
-        void CarryOutPickupActions(Transform tParent)
-        {
-            transform.SetParent(tParent);
-            itemMaster.CallEventObjectPickup();
-            transform.gameObject.SetActive(false);
-        }
-    }
-}
 
