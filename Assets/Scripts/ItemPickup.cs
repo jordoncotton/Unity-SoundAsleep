558db44285278e1c138413ceaf24fb79e34dc589
jordoncotton <42353525+lodisperkins@users.noreply.github.com>
2020-05-15 20:41:11 -0500

    Just need to tweak some issues with holding the items in the inventory

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class ItemPickup : MonoBehaviour
-//{
-//    private Inventory inventory;
-//    public    
-//}
+namespace Cotton
+{
+    public class ItemPickup : MonoBehaviour
+    {
+        private ItemMaster itemMaster;
+
+        void OnEnable()
+        {
+            SetInitialReferences();
+            itemMaster.EventPickupAction += CarryOutPickupActions;
+        }
+
+        void OnDisable()
+        {
+            itemMaster.EventPickupAction -= CarryOutPickupActions;
+        }
+
+        void SetInitialReferences()
+        {
+            itemMaster = GetComponent<ItemMaster>();
+        }
+
+        void CarryOutPickupActions(Transform tParent)
+        {
+            transform.SetParent(tParent);
+            itemMaster.CallEventObjectPickup();
+            transform.gameObject.SetActive(false);
+        }
+    }
+}
+
 
 
