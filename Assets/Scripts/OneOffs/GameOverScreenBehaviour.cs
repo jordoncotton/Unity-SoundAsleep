32bd4c665662058d2bd13e7141b5acb8d0b35b2e
j.micah.manzanares <j.micah.manzanares@gmail.com>
2020-06-18 15:56:11 -0500

    GameOver screen  works and is ready for  art

---
+++ Assets/Scripts/OneOffs/GameOverScreenBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreenBehaviour : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Respawn()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
