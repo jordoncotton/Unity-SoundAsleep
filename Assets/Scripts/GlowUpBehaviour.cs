6d708082a596414333b1c73e53ca6ac9c18525ba
Juan Micah Manzanares <micahmanzanares@gmail.com>
2020-06-30 18:01:05 -0500

    Started Glow UP scripts

---
+++ Assets/Scripts/GlowUpBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowUpBehaviour : MonoBehaviour
+{
+
+    public bool Glow;
+
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
