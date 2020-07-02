3db5c6e0c6e7059e0f3b8ab45bc6ac8699a83a06
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2020-07-02 14:04:15 -0500

    Added enemy character and also sound for the music box but monster isnt responding to sound

---
+++ Assets/Scripts/PlayOnClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnClick : MonoBehaviour
+{
+    public AudioSource Sound;
+    public float coolDownTimer;
+    public float sound = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+
+        if (coolDownTimer < 0)
+        {
+            coolDownTimer = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && coolDownTimer == 0)
+        {
+            Sound.Play();
+            coolDownTimer = sound;
+        }
+    }
+}
