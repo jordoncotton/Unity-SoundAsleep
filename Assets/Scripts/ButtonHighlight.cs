b24b30fd2a20f1ba9a7b77b88c755a452f711901
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2020-07-10 14:30:02 -0500

    added all fonts to the UI and added scripts to each object

---
+++ Assets/Scripts/ButtonHighlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    Text text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
